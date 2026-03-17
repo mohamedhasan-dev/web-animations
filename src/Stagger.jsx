23b2b8260a9fe4febad3d0ac7bb8356706411cbe
Your Name <you@example.com>
2026-03-17 20:46:35 +0530

    scrollTrigger

--- src/Stagger.jsx
+++ src/Stagger.jsx
@@ -4,68 +4,39 @@ const Stagger = () => {
   const tl1 = gsap.timeline();
   function appear() {
     if (!tl1.isActive()) {
-      tl1.clear()
+      tl1.clear();
       tl1.to(".box", {
         scale: 3,
-        duration: 0.1,
         stagger: {
-          grid: [6, 5],
-          amount: 2,
-          from: "random",
+          grid: [17, 15],
+          amount: 1,
+          from: "center",
         },
       });
     }
   }
   function fade() {
     if (!tl1.isActive()) {
-      tl1.clear()
+      tl1.clear();
       tl1.to(".box", {
         scale: 1,
-        duration: 0.1,
         stagger: {
-          grid: [6, 5],
-          amount: 2,
-          from: "random",
+          grid: [17, 15],
+          amount: 1,
+          from: "center",
         },
       });
     }
   }
-
+  const amnt = Array.from({ length: 255 });
   return (
     <>
       <div className="justify-center flex">
-        <div className="grid grid-cols-5 gap-1 w-55 justify-center">
-          <div className="w-3 h-3 bg-green-400 m-3 box"></div>
-          <div className="w-3 h-3 bg-green-400 m-3 box"></div>
-          <div className="w-3 h-3 bg-green-400 m-3 box"></div>
-          <div className="w-3 h-3 bg-green-400 m-3 box"></div>
-          <div className="w-3 h-3 bg-green-400 m-3 box"></div>
-          <div className="w-3 h-3 bg-green-400 m-3 box"></div>
-          <div className="w-3 h-3 bg-green-400 m-3 box"></div>
-          <div className="w-3 h-3 bg-green-400 m-3 box"></div>
-          <div className="w-3 h-3 bg-green-400 m-3 box"></div>
-          <div className="w-3 h-3 bg-green-400 m-3 box"></div>
-          <div className="w-3 h-3 bg-green-400 m-3 box"></div>
-          <div className="w-3 h-3 bg-green-400 m-3 box"></div>
-          <div className="w-3 h-3 bg-green-400 m-3 box"></div>
-          <div className="w-3 h-3 bg-green-400 m-3 box"></div>
-          <div className="w-3 h-3 bg-green-400 m-3 box"></div>
-          <div className="w-3 h-3 bg-green-400 m-3 box"></div>
-          <div className="w-3 h-3 bg-green-400 m-3 box"></div>
-          <div className="w-3 h-3 bg-green-400 m-3 box"></div>
-          <div className="w-3 h-3 bg-green-400 m-3 box"></div>
-          <div className="w-3 h-3 bg-green-400 m-3 box"></div>
-          <div className="w-3 h-3 bg-green-400 m-3 box"></div>
-          <div className="w-3 h-3 bg-green-400 m-3 box"></div>
-          <div className="w-3 h-3 bg-green-400 m-3 box"></div>
-          <div className="w-3 h-3 bg-green-400 m-3 box"></div>
-          <div className="w-3 h-3 bg-green-400 m-3 box"></div>
-          <div className="w-3 h-3 bg-green-400 m-3 box"></div>
-          <div className="w-3 h-3 bg-green-400 m-3 box"></div>
-          <div className="w-3 h-3 bg-green-400 m-3 box"></div>
-          <div className="w-3 h-3 bg-green-400 m-3 box"></div>
-          <div className="w-3 h-3 bg-green-400 m-3 box"></div>
-          <div className="col-span-5 flex justify-center">
+        <div className="grid grid-cols-15 gap-1 w-3xl justify-center">
+          {amnt.map(() => (
+            <div className="w-3 h-3 bg-green-400 m-3 box"></div>
+          ))}
+          <div className="col-span-15 flex justify-center">
             <button
               onClick={appear}
               className="border border-gray-400 p-1.5 m-2"
--- src/app.jsx
+++ src/app.jsx
@@ -1,47 +1,103 @@
 import gsap from "gsap";
 import Stagger from "./Stagger";
-
-
+import { useGSAP } from "@gsap/react";
+import ScrollTrigger from "gsap/ScrollTrigger";
+gsap.registerPlugin(ScrollTrigger);
 const App = () => {
-  const timeLine1 = gsap.timeline();
-  const timeLine2 = gsap.timeline();
-  function play() {
-    timeLine1
-      .to("#green-box", { x: 200, duration: 1, ease: "elastic.inOut" })
-      .to("#blue-box", { x: 200, duration: 2 });
-  }
-  function play2() {
-    timeLine2
-      .to("#red-box", { x: 200, duration: 1, ease: "elastic.inOut" })
-      .to("#yello-box", { x: 200, duration: 2 }, "+=1");
-  }
-
+  useGSAP(() => {
+    gsap.to("#green-box", {
+      x: 1000,
+      rotate: 720,
+      borderRadius: "100%",
+      scrollTrigger: {
+        trigger: "#green-box",
+        start: "bottom 70%",
+        end: "+=1000",
+        markers: true,
+        scrub: 2,
+      },
+    });
+    gsap.to("#blue-box", {
+      x: 1000,
+      rotate: 720,
+      borderRadius: "100%",
+      scrollTrigger: {
+        trigger: "#green-box",
+        start: "+=200 70%",
+        end: "+=1000",
+        markers: true,
+        scrub: 2,
+      },
+    });
+    gsap.from("#red-box", {
+      y: 1000,
+      opacity: 0,
+      duration: 1,
+      scrollTrigger: {
+        trigger: "#yello-box",
+        start: "-=1000 bottom",
+        markers: true,
+        scrub:true
+      },
+    });
+    gsap.from("#yello-box", {
+      y: 1000,
+      opacity: 0,
+      duration: 1,
+      scrollTrigger: {
+        trigger: "#yello-box",
+        start: "-=1000 bottom",
+        markers: true,
+        scrub:true
+      },
+    });
+  }, []);
   return (
     <>
-      <div className="m-2 p-2 border-2 border-white">
-        <div
-          id="green-box"
-          className="w-20 h-20 bg-green-600 m-5 rounded-lg"
-        ></div>
-        <div
-          id="blue-box"
-          className="w-20 h-20 bg-sky-400 m-5 rounded-sm"
-        ></div>
-        <button className="p-1 bg-cyan-500 rounded-sm m-5" onClick={play}>
-          play
-        </button>
-      </div>
-      <div className="m-2 p-2 border-2 border-white">
-        <div id="red-box" className="w-20 h-20 bg-red-600 m-5 rounded-lg"></div>
+      <Stagger />
+      <div
+        id="green-box"
+        className="w-20 h-20 bg-green-600 m-5 rounded-lg"
+      ></div>
+      <div id="blue-box" className="w-20 h-20 bg-sky-400 m-5 rounded-sm"></div>{" "}
+      <div className="flex justify-center">
         <div
           id="yello-box"
-          className="w-20 h-20 bg-amber-300 m-5 rounded-sm"
+          className="w-20 h-20 bg-amber-300 m-5 rounded-sm justify-center"
         ></div>
-        <button className="p-1 bg-cyan-500 rounded-sm m-5" onClick={play2}>
-          play
-        </button>
+        <div id="red-box" className="w-20 h-20 bg-red-600 m-5 rounded-lg"></div>
       </div>
-      <Stagger />
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Sapiente
+        repellat in eaque perspiciatis numquam, quaerat architecto. Voluptatem
+        harum, accusamus rem accusantium enim consectetur, magni voluptatum
+        expedita fugit facilis nobis perferendis.
+        <br />
+        <br />
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Totam,
+        quibusdam omnis dolor expedita nemo quo, repudiandae laudantium debitis
+        saepe eaque facilis quos deserunt exercitationem distinctio sed ipsa
+        error quis reprehenderit!
+        <br />
+        <br />
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Libero
+        inventore dicta, iure odio molestias omnis quo atque aut, aliquid soluta
+        itaque maiores velit consequuntur. Pariatur, expedita rem praesentium ad
+        recusandae sapiente dolorem aliquam iste fuga?
+        <br />
+        <br />
+        Voluptates delectus ratione, tempore atque, animi culpa ullam tenetur
+        nemo recusandae vero illum molestias dicta quod exercitationem suscipit
+        numquam ipsum minima in soluta doloremque labore inventore. Facilis,
+        totam alias, expedita, quidem veniam esse laborum
+        <br />
+        <br />
+        veritatis nobis est quod ab? Id atque explicabo blanditiis at dolores
+        voluptate quasi perferendis, excepturi corporis labore ut iusto impedit
+        velit sint necessitatibus assumenda delectus numquam, provident, ex
+        tempore repellat dignissimos.
+        <br />
+      </p>
     </>
   );
 };
